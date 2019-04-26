480eacbc89d1c31319066152aeebd9e3f4985b10
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-04-26 10:51:31 +0200

    more decorator

--- lesson 18/simple_decorator.py
+++ lesson 18/simple_decorator.py
@@ -1,18 +1,36 @@
 def my_decorator(func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         print("This code is executed first.")
-        func()
+        func(*args, **kwargs)
         print("This code is executed last.")
 
     return wrapper
 
 
 @my_decorator
-def say_whee():
-    print("Whee!")
+def say_whee(name):
+    print(f'Whee! {name}')
 
 
-whee = my_decorator(say_whee)
+@my_decorator
+def greet():
+    print("Static method")
+
+
+def do_twice(func):
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        func(*args, **kwargs)
+
+    return wrapper_do_twice
+
+
+@do_twice
+def greet_twice(name):
+    print(f'Hello {name}!')
+
 
 if __name__ == '__main__':
-    whee()
+    say_whee("Frank")
+    greet()
+    greet_twice("Bob")
